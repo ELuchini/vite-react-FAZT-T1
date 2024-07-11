f38fad3c6fd69f26de5ccb1688eb58f0ef8c74c1
eluchini <eluchini@outlook.com>
2024-07-10 23:22:08 -0300

    Cambios menores CSS

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -13,7 +13,7 @@ function TaskCard({ task }) {
   }
 
   return (
-    <div className="bg-gray-800 text-white rounded-md">
+    <div className="bg-gray-800 text-white rounded-md border-4 border-green-600 border-double p-2">
       <h1 className="text-xl font-bold capitalize">{task.title}</h1>
       <p className="text-gray-300 text-lg">{task.description}</p>
       <button className="bg-red-800 px-2 py-1 rounded-md mt-4 hover:bg-red-300" onClick={() => deleteTask(task.id)}>Eliminar Tarea</button>
